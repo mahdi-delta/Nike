ea3371e2129a19e45e3f87142e49e6db62a2c621
mohammad mahdi ghorbani <mohammadmahdi271uni@gmail.com>
2025-11-11 15:09:50 +0330

    fix bugs in responsive design

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -17,12 +17,12 @@ const Hero = () => {
                className="w-full flex xl:flex-row flex-col
               justify-center min-h-screen gap-10 max-container"
           >
-               <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28 max-sm:text-4xl max-sm:items-center">
-                    <p className="text-lg sm:text-xl font-montserrat text-coral-red">
+               <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 max-sm:items-center">
+                    <p className="w-full text-lg sm:text-xl flex justify-start items-center font-montserrat text-coral-red pb-2">
                          Our Summer Collection
                     </p>
 
-                    <h1 className="mt-10 font-palanquin text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold leading-tight sm:leading-[82px]">
+                    <h1 className="mt-10 font-palanquin text-8xl maxlg:text-6xl max-md:text-4xl font-bold leading-tight sm:leading-[82px]">
                          <span
                               className="xl:bg-white xl:whitespace-nowrap relative
                               z-10 pr-10"
@@ -35,14 +35,14 @@ const Hero = () => {
 
                     <p
                          className="font-montserrat text-slate-gray text-base sm:text-lg
-                         leading-7 sm:leading-8 mt-6 mb-14 sm:max-w-sm"
+                         leading-7 sm:leading-8 mt-6 mb-14 sm:max-w-sm max-sm:text-start"
                     >
                          Discover Stylish Nike arrivals, quality comfort, and innovation for your
                          active life.
                     </p>
                     <Button label="shop now" iconURl={arrowRight} />
 
-                    <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-8 sm:gap-16">
+                    <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-8 sm:gap-16 max-sm:justify-center">
                          {statistics.map((stat, index) => (
                               <div key={stat.label}>
                                    <p className="text-2xl sm:text-3xl lg:text-4xl font-palanquin font-bold ">
